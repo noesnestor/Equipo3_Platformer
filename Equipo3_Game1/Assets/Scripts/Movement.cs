a5cc27d1e6b21ac2ec3e97d39f5abf57464488d5
noesnestor <nestor.marvi@gmail.com>
2023-06-01 17:40:02 -0300

    V1

--- Equipo3_Game1/Assets/Scripts/Movement.cs
+++ Equipo3_Game1/Assets/Scripts/Movement.cs
@@ -7,25 +7,47 @@ public class Movement : MonoBehaviour
     [SerializeField] float velocidad;
     [SerializeField] float fuerzaSalto;
     [SerializeField] LayerMask suelo;
+    [SerializeField] float velocidadSlide;
+    private bool facingRight;
     private Rigidbody2D personaje;
-    private float horizontalInput;
+
     private BoxCollider2D boxCollider;
     // Start is called before the first frame update
     void Start()
     {
         personaje = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
-
+        facingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        personaje.velocity = new Vector2(horizontalInput*velocidad,personaje.velocity.y);
-        if(Input.GetKeyDown(KeyCode.UpArrow) && estaEnElSuelo()){
+        if(Input.GetKey(KeyCode.A)){
+            personaje.velocity = new Vector2(-1f*velocidad,personaje.velocity.y);
+            facingRight = false;
+        }
+        
+
+        else if(Input.GetKey(KeyCode.D)){
+           personaje.velocity = new Vector2(1f*velocidad,personaje.velocity.y); 
+           facingRight = true;
+        }
+
+        //Jump
+        if(Input.GetKeyDown(KeyCode.Space) && estaEnElSuelo()){
             personaje.AddForce(Vector2.up*fuerzaSalto,ForceMode2D.Impulse);
-        } 
+        }
+
+        //Slide
+        if(Input.GetKeyDown(KeyCode.LeftControl) && estaEnElSuelo()){
+            if(facingRight){
+                personaje.AddForce(Vector2.right*velocidadSlide,ForceMode2D.Impulse);
+            }
+            else if (!facingRight){
+                personaje.AddForce(Vector2.left*velocidadSlide,ForceMode2D.Impulse);
+            }
+        }
     }
 
     private bool estaEnElSuelo(){
